7d18fe9f932b08af2cd248b6b01910d57163bb96
Sholana1 <shodunkedavid16@gmail.com>
2026-04-14 05:37:29 +0100

    feat: add logging with loguru

--- backend/app/api/routes/home.py
+++ backend/app/api/routes/home.py
@@ -1,7 +1,14 @@
 from fastapi import APIRouter
+from app.core.logging import get_logger
+
+logger = get_logger()
 
 router = APIRouter(prefix="/home")
 
 @router.get("/")
 def home():
+    logger.info("Home page accessed")
+    logger.debug("Home page accessed")
+    logger.warning("Home page accessed")
+    logger.error("Home page accessed")
     return {"message": "Welcome to the flow bank api"}
