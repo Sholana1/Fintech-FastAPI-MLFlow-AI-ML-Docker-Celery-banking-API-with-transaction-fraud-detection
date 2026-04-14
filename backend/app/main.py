24f7fa2a8bdbe279c7b8329b46a8bc2f5dd06515
Sholana1 <shodunkedavid16@gmail.com>
2026-04-14 04:53:15 +0100

    chore: add env variable and config setting

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
+from .api.main import api_router
+from .core.config import settings
 
 app = FastAPI(
-    title="Flow Bank",
-    description="Flow banking API built with FastAPI"
+    title=settings.PROJECT_NAME,
+    description=settings.PROJECT_DESCRIPTION,
+    docs_url=f"{settings.API_V1_STR}/docs",
+    redoc_url=f"{settings.API_V1_STR}/redoc",
+    openapi_url=f"{settings.API_V1_STR}/openapi.json"
 )
 
-@app.get("/")
-def home():
-    return {"message": "Welcome to the flow bank api"}
+app.include_router(api_router, prefix=settings.API_V1_STR)
+
